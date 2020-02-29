32c097c27c2ba9813b88454d97807ad1e98045e5
Alexander Zavyalov <whmordred@gmail.com>
2020-02-29 12:17:25 +0800

    Clear config cache before running PHPUnit tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,33 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        if ($app->configurationIsCached()) {
+            Artisan::call('config:clear');
+
+            return $this->createApplication();
+        }
+
+        return $app;
+    }
     
     protected function setUp(): void
     {
