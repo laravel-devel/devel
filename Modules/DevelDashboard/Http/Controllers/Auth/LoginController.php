44202c92584f6496e466bf77d6d794c514193abf
Alexander Zavyalov <whmordred@gmail.com>
2020-03-03 13:31:57 +0800

    Dashboard - users without the "admin_dashboard.access" cannot log in

--- Modules/DevelDashboard/Http/Controllers/Auth/LoginController.php
+++ Modules/DevelDashboard/Http/Controllers/Auth/LoginController.php
@@ -50,6 +50,48 @@ class LoginController extends Controller
     }
 
     /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (
+            method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)
+        ) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            // If the user is not allowed to access the Dashboard - log them
+            // out, return an error
+            if (auth()->user()->hasPermissions('admin_dashboard.access')) {
+                return $this->sendLoginResponse($request);
+            }
+
+            $this->guard()->logout();
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /**
      * Send the response after the user was authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
