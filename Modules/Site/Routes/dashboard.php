b84ba25853b06f541ad9b62f8a95b00fb501801b
Alexander Zavyalov <whmordred@gmail.com>
2020-03-05 14:28:18 +0800

    Removed unused dashboard routes from Site module

--- Modules/Site/Routes/dashboard.php
+++ Modules/Site/Routes/dashboard.php
@@ -3,19 +3,5 @@
 Route::group([
     'prefix' => config('develdashboard.dashboard_uri') . '/' . config('site.slug'),
 ], function() {
-    /**
-     * Module settings
-     */
-    Route::get('/settings', [
-        'as' => 'site.settings.edit',
-        'uses' => 'SettingsController@edit',
-        'dashboardMenu' => 'Site->Settings',
-        'permissions' => 'site.edit-settings',
-    ]);
-
-    Route::post('/settings', [
-        'as' => 'site.settings.update',
-        'uses' => 'SettingsController@update',
-        'permissions' => 'site.edit-settings',
-    ]);
+    //
 });
