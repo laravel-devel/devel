32036783d3fb13fc9286863ba2aaa3efda5aec8e
Alexander Zavyalov <whmordred@gmail.com>
2020-03-05 14:43:22 +0800

    Stripped down the resources folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,9 +3,3 @@ const mix = require('laravel-mix');
 // Allow multiple Laravel Mix applications
 require('laravel-mix-merge-manifest');
 mix.mergeManifest();
-
-// mix.sass('resources/sass/dashboard.scss', 'public/css')
-//     .js('resources/js/dashboard.js', 'public/js')
-//     .copy('resources/sass/dashboard/line-awesome.min.css', 'public/css/line-awesome.min.css')
-//     .copyDirectory('resources/sass/dashboard/fonts', 'public/fonts')
-//     .version();
