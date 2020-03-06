a595253108af3806bb01352904df409996eb0358
Alexander Zavyalov <whmordred@gmail.com>
2020-03-06 18:40:50 +0800

    Moved the exception handler class to the Main module

--- Modules/Main/ExceptionHandler.php
+++ Modules/Main/ExceptionHandler.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Exceptions;
+namespace Modules\Main;
 
 use Exception;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Foundation\Exceptions\Handler;
 
-class Handler extends ExceptionHandler
+class ExceptionHandler extends Handler
 {
     /**
      * A list of the exception types that are not reported.
