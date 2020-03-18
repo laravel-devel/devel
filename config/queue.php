917b9233c1b201841e0ca4ab03cf4ea317ddc1a0
Alexander Zavyalov <whmordred@gmail.com>
2020-03-18 10:30:09 +0800

    Prefixed some default DB tables with `_`

--- config/queue.php
+++ config/queue.php
@@ -36,7 +36,7 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'table' => 'jobs',
+            'table' => '_jobs',
             'queue' => 'default',
             'retry_after' => 90,
         ],
@@ -82,7 +82,7 @@ return [
     'failed' => [
         'driver' => env('QUEUE_FAILED_DRIVER', 'database'),
         'database' => env('DB_CONNECTION', 'mysql'),
-        'table' => 'failed_jobs',
+        'table' => '_failed_jobs',
     ],
 
 ];
--- devel/core/Database/Migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ devel/core/Database/Migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,7 +13,7 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('_failed_jobs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->text('connection');
             $table->text('queue');
@@ -30,6 +30,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('_failed_jobs');
     }
 }
