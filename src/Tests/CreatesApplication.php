d76c01a545cdbcc907112ed5e489d1de5a7a4575
Alexander Zavyalov <whmordred@gmail.com>
2020-04-03 14:08:52 +0800

    Fixed a file path in Tests/CreatesApplication

--- src/Tests/CreatesApplication.php
+++ src/Tests/CreatesApplication.php
@@ -13,7 +13,7 @@ trait CreatesApplication
      */
     public function createApplication()
     {
-        $app = require base_path('bootstrap/app.php');
+        $app = require __DIR__ . '/../../../../../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
 
